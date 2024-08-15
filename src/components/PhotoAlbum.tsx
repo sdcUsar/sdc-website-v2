5822ea30de399c6e8fe56bab56b96eed3b47b729
Yusuf <mdyusuf2521@gmail.com>
2024-08-15 17:03:34 +0530

    Deleted module.style.css

--- src/components/PhotoAlbum.tsx
+++ src/components/PhotoAlbum.tsx
@@ -1,211 +1,208 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 const PhotoAlbum = ({ className }: { className?: string }) => {
-
   return (
     <section
       className={cn(
         "relative w-full flex justify-center items-center text-white flex-col h-[80vh] px-4 mb-14 mt-auto"
       )}
     >
-      <div className="absolute z-0 text-6xl font-extrabold" >Life At <span className="text-blue-500">SDC</span></div>
+      <div className="absolute z-0 text-6xl font-extrabold">
+        Life At <span className="text-blue-500">SDC</span>
+      </div>
 
-      <div
-        className="z-10 w-full max-h-full flex flex-row flex-wrap justify-center items-center overflow-hidden gap-y-4 gap-3"
-      >
-
-        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none "
-        >
+      <div className="z-10 w-full max-h-full flex flex-row flex-wrap justify-center items-center overflow-hidden gap-y-4 gap-3">
+        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none ">
           <Image
             src="/testpic.png"
             width={500}
             height={500}
             alt="album_pic"
-            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square " />
-        </div>
-
-        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none "
-        >
-          <Image
-            src="/testpic.png"
-            width={500}
-            height={500}
-            alt="album_pic"
-            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square " />
-        </div>
-        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none "
-        >
-          <Image
-            src="/testpic.png"
-            width={500}
-            height={500}
-            alt="album_pic"
-            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square " />
-        </div>
-        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none "
-        >
-          <Image
-            src="/testpic.png"
-            width={500}
-            height={500}
-            alt="album_pic"
-            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square " />
-        </div>
-        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none "
-        >
-          <Image
-            src="/testpic.png"
-            width={500}
-            height={500}
-            alt="album_pic"
-            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square " />
-        </div>
-        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none "
-        >
-          <Image
-            src="/testpic.png"
-            width={500}
-            height={500}
-            alt="album_pic"
-            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square " />
-        </div>
-        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none "
-        >
-          <Image
-            src="/testpic.png"
-            width={500}
-            height={500}
-            alt="album_pic"
-            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square " />
-        </div>
-        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none "
-        >
-          <Image
-            src="/testpic.png"
-            width={500}
-            height={500}
-            alt="album_pic"
-            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square " />
-        </div>
-        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none "
-        >
-          <Image
-            src="/testpic.png"
-            width={500}
-            height={500}
-            alt="album_pic"
-            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square " />
-        </div>
-        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none "
-        >
-          <Image
-            src="/testpic.png"
-            width={500}
-            height={500}
-            alt="album_pic"
-            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square " />
+            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square "
+          />
         </div>
 
-        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none "
-        >
+        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none ">
           <Image
             src="/testpic.png"
             width={500}
             height={500}
             alt="album_pic"
-            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square " />
+            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square "
+          />
         </div>
-        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none "
-        >
+        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none ">
           <Image
             src="/testpic.png"
             width={500}
             height={500}
             alt="album_pic"
-            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square " />
+            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square "
+          />
         </div>
-        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none "
-        >
+        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none ">
           <Image
             src="/testpic.png"
             width={500}
             height={500}
             alt="album_pic"
-            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square " />
+            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square "
+          />
         </div>
-        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none "
-        >
+        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none ">
+          <Image
+            src="/testpic.png"
+            width={500}
+            height={500}
+            alt="album_pic"
+            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square "
+          />
+        </div>
+        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none ">
+          <Image
+            src="/testpic.png"
+            width={500}
+            height={500}
+            alt="album_pic"
+            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square "
+          />
+        </div>
+        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none ">
+          <Image
+            src="/testpic.png"
+            width={500}
+            height={500}
+            alt="album_pic"
+            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square "
+          />
+        </div>
+        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none ">
+          <Image
+            src="/testpic.png"
+            width={500}
+            height={500}
+            alt="album_pic"
+            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square "
+          />
+        </div>
+        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none ">
+          <Image
+            src="/testpic.png"
+            width={500}
+            height={500}
+            alt="album_pic"
+            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square "
+          />
+        </div>
+        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none ">
+          <Image
+            src="/testpic.png"
+            width={500}
+            height={500}
+            alt="album_pic"
+            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square "
+          />
+        </div>
+
+        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none ">
+          <Image
+            src="/testpic.png"
+            width={500}
+            height={500}
+            alt="album_pic"
+            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square "
+          />
+        </div>
+        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none ">
+          <Image
+            src="/testpic.png"
+            width={500}
+            height={500}
+            alt="album_pic"
+            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square "
+          />
+        </div>
+        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none ">
+          <Image
+            src="/testpic.png"
+            width={500}
+            height={500}
+            alt="album_pic"
+            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square "
+          />
+        </div>
+        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none ">
           <Image
             src="/testpic.png"
             width={300}
             height={300}
             alt="album_pic"
-            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square " />
+            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square "
+          />
         </div>
-        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none "
-        >
+        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none ">
           <Image
             src="/testpic.png"
             width={500}
             height={500}
             alt="album_pic"
-            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square " />
+            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square "
+          />
         </div>
-        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none "
-        >
+        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none ">
           <Image
             src="/testpic.png"
             width={500}
             height={500}
             alt="album_pic"
-            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square " />
+            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square "
+          />
         </div>
-        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none "
-        >
+        <div className=" h-[30%] transition-all aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none ">
           <Image
             src="/testpic.png"
             width={500}
             height={500}
             alt="album_pic"
-            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square " />
+            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square "
+          />
         </div>
-        <div className=" h-[30%] transition-[all 0.5s]  aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none "
-        >
+        <div className=" h-[30%] transition-[all 0.5s]  aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none ">
           <Image
             src="/testpic.png"
             width={500}
             height={500}
             alt="album_pic"
-            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square " />
+            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square "
+          />
         </div>
-        <div className=" h-[30%] transition-[all 0.5s]  aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none "
-        >
+        <div className=" h-[30%] transition-[all 0.5s]  aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none ">
           <Image
             src="/testpic.png"
             width={500}
             height={500}
             alt="album_pic"
-            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square " />
+            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square "
+          />
         </div>
-        <div className=" h-[30%] transition-[all 0.5s]  aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none "
-        >
+        <div className=" h-[30%] transition-[all 0.5s]  aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none ">
           <Image
             src="/testpic.png"
             width={500}
             height={500}
             alt="album_pic"
-            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square " />
+            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square "
+          />
         </div>
-        <div className=" h-[30%] transition-[all 0.5s]  aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none "
-        >
+        <div className=" h-[30%] transition-[all 0.5s]  aspect-square basis-[1/3] rounded-xl object-cover opacity-0 -translate-y-[30px] scale-100 hover:opacity-100 hover:transform-none ">
           <Image
             src="/testpic.png"
             width={500}
             height={500}
             alt="album_pic"
-            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square " />
+            className="transition-all object-cover rounded-[100%] hover:rounded-xl aspect-square "
+          />
         </div>
-
       </div>
     </section>
   );
