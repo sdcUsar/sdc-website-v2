e58fed53b389a55a52bc4f2e8d4d180bc759e683
tanishq9304@gmail.com <tanishq9304@gmail.com>
2024-08-22 04:50:14 +0530

    re initial commit tp production

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import withVideos from 'next-videos';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withVideos({
-    distDir: 'build',
+
   webpack(config, options) {
     return config;
   },
